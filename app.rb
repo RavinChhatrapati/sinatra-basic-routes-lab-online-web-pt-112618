1f06850367dc642e5471619d99c2090bea883260
Ravin Chhatrapati <ravinchhatrapati95@gmail.com>
2019-03-14 00:57:56 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-end
+
+  get '/name' do
+    "My name is Ravin"
+  end
+    get '/hometown' do
+    "My hometown is Texas"
+  end
+    get '/favorite-song' do
+      "My favorite song is Thriller"
+    end
+    end
