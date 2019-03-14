9cee830fded8a0092c822da130c0b8b5f00053e6
Ravin Chhatrapati <ravinchhatrapati95@gmail.com>
2019-03-14 00:50:14 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,12 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Ravin"
+    get ''/hometown' do
+    My hometown is Texas  
+    get '/favorite-song' do
+      "My favorite song is Thriller"
+    end  
+    end
 end
